38b20d19c9bce71f2616c596bfddc290543489b0
sai prasad <kondaparthisaiprasad117@gmail.com>
2022-12-12 10:50:13 +0530

    msg4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,14 +2,14 @@ package com.driver;
 
 public class Main {
 public static class Product{
-    public int product(int x, int y) {
+    public static int product(int x, int y) {
 
         return x+y;
     }
-    public int product(int x, int y, int z) {
+    public static int product(int x, int y, int z) {
         return x*y+z;
     }
-    public double product(double x, double y) {
+    public static double product(double x, double y) {
         return x/y;
     }
 
